671a408a9fd2735fc8c0309f8b41bda0741fa8b6
Utsab Kumar Agrawal <52622694+Utsav77@users.noreply.github.com>
2020-07-08 19:56:02 +0530

    Add files via upload

---
+++ 3sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeSum(self, nums):
+        nums.sort()
+        n, result = len(nums), []
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]: continue
+
+            target = -nums[i]
+            beg, end = i + 1, n - 1
+
+            while beg < end:
+                if nums[beg] + nums[end] < target:
+                    beg += 1
+                elif nums[beg] + nums[end] > target:
+                    end -= 1
+                else:
+                    result.append((nums[i], nums[beg], nums[end]))
+                    beg += 1
+                    end -= 1
+        return set(result)
