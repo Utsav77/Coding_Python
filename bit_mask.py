ed885b187c8d92f6b1b8b315728a3a4a28baefbd
Utsab Kumar Agrawal <52622694+Utsav77@users.noreply.github.com>
2020-07-11 19:44:46 +0530

    Add files via upload

---
+++ bit_mask.py
@@ -0,0 +1,3 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        return [ [nums[i] for i, bit in enumerate(bin(mask)[-1:1:-1]) if bit=='1'] for mask in range(1<<len(nums))]
